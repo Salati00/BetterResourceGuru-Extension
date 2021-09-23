f925bb04de0ddde455b545bd46d4d927325621d2
Salati00 <salati00.giorgio@gmail.com>
2021-09-23 13:50:00 +0200

    REMOVE console.info, ADD check for undefined delay

--- inject.js
+++ inject.js
@@ -3,11 +3,10 @@
   chrome.storage.sync.get(["delayMs"], function (delay) {
     console.info('Bettering Guru')
     setTimeout(function () {
-      console.info(delay);
       inspQuote = document.getElementsByClassName('guru__quote_2l78Zu_zvR')[0]
       if (inspQuote != undefined) {
         inspQuote.remove()
       }
-    }, parseInt(delay.delayMs))
+    }, ((delay == undefined) ? 5 : parseInt(delay.delayMs)))
   })
 })()
